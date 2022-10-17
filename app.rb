4df95ce2b14adcaac99147f0bdd0410982dbb67f
Léo Hetsch <leo.hetsch@makers.tech>
2022-10-17 15:50:54 +0100

    Update Gemfile.lock for deploy

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require "sinatra/reloader"
 require_relative 'lib/database_connection'
 require_relative 'lib/album_repository'
 
-DatabaseConnection.connect('music_library')
+# DatabaseConnection.connect('music_library')
 
 class Application < Sinatra::Base
   configure :development do
@@ -12,10 +12,14 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository.rb'
   end
 
-  get '/albums' do
-    album_repository = AlbumRepository.new
-    @albums = album_repository.all
-
-    return erb(:index)
+  get '/' do
+    return 'Hello'
   end
+
+  # get '/albums' do
+  #   album_repository = AlbumRepository.new
+  #   @albums = album_repository.all
+
+  #   return erb(:index)
+  # end
 end
