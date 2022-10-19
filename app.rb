d65b7a47f0301b0efe7be1bf2a87428fb4c0b134
Léo Hetsch <leo.hetsch@makers.tech>
2022-10-19 11:51:37 +0100

    Setup for database in Render

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require "sinatra/reloader"
 require_relative 'lib/database_connection'
 require_relative 'lib/album_repository'
 
-# DatabaseConnection.connect('music_library')
+DatabaseConnection.connect('music_library')
 
 class Application < Sinatra::Base
   configure :development do
@@ -16,10 +16,10 @@ class Application < Sinatra::Base
     return 'Hello'
   end
 
-  # get '/albums' do
-  #   album_repository = AlbumRepository.new
-  #   @albums = album_repository.all
+  get '/albums' do
+    album_repository = AlbumRepository.new
+    @albums = album_repository.all
 
-  #   return erb(:index)
-  # end
+    return erb(:index)
+  end
 end
